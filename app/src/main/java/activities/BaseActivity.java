bf5a36403f60b167416ba052987d4cc7a840c567
zpw <zpw10018@ly.com>
2016-09-15 16:05:21 +0800

    zpw-2016-9-15 1修改类路径

---
+++ app/src/main/java/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zpw on 2016/9/15.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package activities;
+
+import android.os.Bundle;
+
+import com.cool.zpw10018.activities.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
