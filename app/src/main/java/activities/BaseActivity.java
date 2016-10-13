c2dc2ea42c0548a4902bf34f8d395bef2cb65ff8
zpw <zpw10018@ly.com>
2016-10-13 17:24:36 +0800

    zpw-2016-9-15 1基类抽出公共方法 2主activity添加布局

--- app/src/main/java/activities/BaseActivity.java
+++ app/src/main/java/activities/BaseActivity.java
@@ -1,10 +1,27 @@
 package activities;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by zpw on 2016/9/15.
+ *
+ * 在API22之前我们使用标题栏基本都是在ActionBarActivity的Activity中处理的，而API22之后，谷歌遗弃了ActionBarActivity，推荐我们也可以说是强制我们使用AppCompatActivity
  */
 
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        int layoutId = getLayoutId();
+        setContentView(layoutId);
+        initView();
+    }
+
+
+    public abstract int getLayoutId();
+
+    public abstract void initView();
 }
--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -9,6 +9,15 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView() {
+
     }
 }
