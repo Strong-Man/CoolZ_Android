87482cdbb871562955d2d4f07e8d79178d3e1775
zpw <zpw10018@ly.com>
2016-10-14 18:19:10 +0800

    zpw-2016-9-15 1抽屉类

--- app/src/main/java/activities/BaseActivity.java
+++ app/src/main/java/activities/BaseActivity.java
@@ -2,6 +2,7 @@ package activities;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -12,14 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         int layoutId = getLayoutId();
         setContentView(layoutId);
         initView();
     }
-
 
     public abstract int getLayoutId();
 
--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -1,15 +1,17 @@
 package activities;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.view.ViewGroup;
 
 import com.cool.zpw10018.activities.R;
 
 public class MainActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    private DrawerLayout mDrawerLayout;
+    private ViewGroup mContentLayout;
+    private NavigationView mNavigationView;
 
     @Override
     public int getLayoutId() {
@@ -18,6 +20,14 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
-
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mContentLayout = (ViewGroup) findViewById(R.id.main_content_view);
+        mNavigationView = (NavigationView) findViewById(R.id.main_nav_view);
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 }
