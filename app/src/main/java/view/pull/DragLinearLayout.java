b18c7caeda436f46b268f94a724ce4e11340e035
zpw <130240770@qq.com>
2016-12-19 00:05:59 +0800

    1可以拖拽的linearlayout

--- app/src/main/java/view/pull/DragLinearLayout.java
+++ app/src/main/java/view/pull/DragLinearLayout.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.ViewConfiguration;
 import android.widget.LinearLayout;
 
 /**
@@ -18,32 +19,78 @@ public class DragLinearLayout extends LinearLayout {
     static final int RELEASE_STATE = 0x1;
     static final int NOMAL_STATE = 0x2;
 
-    private int mState;
+    private int mState = NOMAL_STATE;
     private int mLimitDragDownHeight = -1;
     private boolean mDragEnable = true;
+    private boolean mIsBeingDragged;
 
-    private float mLastMotionY, mInitialMotionY, mLastMotionX;
+    private float mLastMotionY, mInitialMotionY, mInitialMotionX, mLastMotionX;
+    private int mTouchSlop;
+
+    private DragDistanceListener mDragDistanceListener;
 
     public DragLinearLayout(Context context) {
         super(context);
+        init();
     }
 
     public DragLinearLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public DragLinearLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
 
     @Override
-    public boolean onInterceptHoverEvent(MotionEvent event) {
-        Log.d(TAG, "onInterceptHoverEvent:----" + "MotionEvent: " + event.getAction());
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onInterceptTouchEvent:----" + "MotionEvent: " + event.getAction());
         if (!mDragEnable || !isCanDragState()) {
-            return super.onInterceptHoverEvent(event);
+            return false;
         }
-        return true;
+
+        final int action = event.getAction();
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mIsBeingDragged = false;
+            return false;
+        }
+
+        if (action != MotionEvent.ACTION_DOWN && mIsBeingDragged) {
+            return true;
+        }
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mLastMotionY = mInitialMotionY = event.getY();
+                mLastMotionX = mInitialMotionX = event.getX();
+                mIsBeingDragged = false;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float y = event.getY(), x = event.getX();
+                final float diff, oppositeDiff, absDiff;
+                diff = y - mLastMotionY;
+                oppositeDiff = x - mLastMotionX;
+                absDiff = Math.abs(diff);
+                if (absDiff > mTouchSlop && (absDiff > Math.abs(oppositeDiff))) {
+                    if (diff >= 1f) {
+                        mLastMotionY = y;
+                        mLastMotionX = x;
+                        mIsBeingDragged = true;
+
+                    } else if (diff <= -1f) {
+                        mLastMotionY = y;
+                        mLastMotionX = x;
+                        mIsBeingDragged = true;
+
+                    }
+                }
+                break;
+        }
+
+        return mIsBeingDragged;
     }
 
     @Override
@@ -61,17 +108,63 @@ public class DragLinearLayout extends LinearLayout {
             case MotionEvent.ACTION_DOWN:
                 mLastMotionY = mInitialMotionY = event.getY();
                 mLastMotionX = event.getX();
-                break;
-//                isBeingDragged = false;
+                return true;
+
             case MotionEvent.ACTION_MOVE:
+                if (true) {
+                    mLastMotionY = event.getY();
+                    mLastMotionX = event.getX();
+                    dragEvent();
+                    return true;
+                }
 
-
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_UP: {
+                if (true) {
+                    mIsBeingDragged = false;
+                    setScroll(0);
+                    return true;
+                }
+                break;
+            }
         }
 
-        return super.onTouchEvent(event);
+        return false;
+    }
+
+    private void init() {
+        ViewConfiguration config = ViewConfiguration.get(getContext());
+        mTouchSlop = config.getScaledTouchSlop();
+    }
+
+    private void dragEvent() {
+        final int newScrollValue;
+        final int itemDimension;
+        final float initialMotionValue, lastMotionValue;
+
+        initialMotionValue = mInitialMotionY;
+        lastMotionValue = mLastMotionY;
+
+        newScrollValue = Math.round(Math.min(initialMotionValue - lastMotionValue, 0) / FRICTION);
+        setScroll(newScrollValue);
+    }
+
+    private void setScroll(int value) {
+        scrollTo(0, value);
+        if (mDragDistanceListener != null) {
+            mDragDistanceListener.onDrag(value);
+        }
     }
 
     private boolean isCanDragState() {
         return mState == NOMAL_STATE;
+    }
+
+    public void setDragDistanceListener(DragDistanceListener dragDistanceListener) {
+        mDragDistanceListener = dragDistanceListener;
+    }
+
+    interface DragDistanceListener {
+        void onDrag(int distanceValue);
     }
 }
